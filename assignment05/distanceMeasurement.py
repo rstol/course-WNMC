d4331fdcc74210104060db415a65896a7415dccf
DavidBohner <davidbohner@protonmail.ch>
2021-12-05 17:03:23 +0100

    hopefully automated package size testing and repetitions

--- assignment05/distanceMeasurement.py
+++ assignment05/distanceMeasurement.py
@@ -12,7 +12,7 @@ def padded(msg, size): #payload size -1 to accomodate for the \0-termination.
     padlen = size - 1
     return msg[(-1) * padlen:].rjust(padlen, '0')
 
-def vizulisation(timestamps):
+def visualization(payload_size, timestamps):
   data_pts = len(timestamps)
   np_timestamps = np.asarray(timestamps)
   throughput = np.zeros(data_pts-1) #data packets per second
@@ -21,19 +21,19 @@ def vizulisation(timestamps):
   delay = np_timestamps[1:] - np_timestamps[:-1] 
   throughput = 1.0/delay
 
-  print("Delay: ", delay)
-  print("Throughput: ", throughput)
-  print("Standard deviation delay: ", np.std(delay))
-  print("Standard deviation throughput: ", np.std(throughput))
+  print(f"[{payload_size}] Delay: ", delay)
+  print(f"[{payload_size}] Throughput: ", throughput)
+  print(f"[{payload_size}] Standard deviation delay: ", np.std(delay))
+  print(f"[{payload_size}] Standard deviation throughput: ", np.std(throughput))
 
-def transmission_control(arduino, recv_addr, payload_size, timestamps):
+def transmission_control(arduino, recv_addr, payload_size, timestamps, repetitions):
   trans_message = 0
   recv_message = ''
 
   send_data(arduino, trans_message, recv_addr)
   timestamps.append(time.time())
 
-  while True:
+  while trans_message < repetitions:
     try: 
       byte = arduino.read(1) #read one byte (blocks until data available or timeout reached) 
       if byte==b'\n': #if termination character reached
@@ -50,7 +50,8 @@ def transmission_control(arduino, recv_addr, payload_size, timestamps):
     except serial.SerialException: 
       continue #on timeout try to read again
 
-arduino = serial.Serial(port='COM4', baudrate=115200, timeout=1) #opens a serial port (resets the device!) 
+
+arduino = serial.Serial(port='/dev/COM4', baudrate=115200, timeout=1) #opens a serial port (resets the device!) 
 time.sleep(2) #give the device some time to startup (2 seconds)
 arduino.write(f'a[AA]\n'.encode()) #set the device address
 time.sleep(0.1) #wait for settings to be applied
@@ -59,14 +60,19 @@ time.sleep(0.1) #wait for settings to be applied
 arduino.write('c[0,1,30]\n'.encode()) #set FEC threshold to 30 (apply FEC to packets with payload >= 30)
 time.sleep(0.1) #wait for settings to be applied
 
-timestamps = []
+timestamps = [[], [], [], [], [], []]
+payload_sizes = [1, 10, 50, 100, 150, 200]
+repetitions = 50
 
-payload_size = int(input('Enter desired packet data size (Byte): '))
+# payload_size = int(input('Enter desired packet data size (Byte): '))
 recv_addr = input('Enter desired receive address: ')
 
 try:
-  transmission_control(arduino, recv_addr, payload_size, timestamps)
+  for i in range(0, len(payload_sizes)): transmission_control(arduino, recv_addr, payload_sizes[i], timestamps[i], repetitions)
 except KeyboardInterrupt:
-  vizulisation(timestamps)
+  print("Interrupted! Continuing with collected data:")
 
-#TODO: run measurements for different packet sizes automatically and plot packet size vs throughput & delay
+for i in range(0, len(payload_sizes)): visualization(payload_sizes[i], timestamps[i])
+
+#TODO: plot packet size vs throughput & delay (for each distance)
+#TODO: maybe add detection for ACK vs TIMEOUT to get better data insight (can also observe receiving device for this)
